8a2bc4abf51a7abf4e222778eb429641dcc517fa
Jesus Chavez Valencia <130213521+jbus3001@users.noreply.github.com>
2026-06-09 22:14:17 -0600

    Add files via upload

---
+++ Blackjack.py
@@ -0,0 +1,65 @@
+import Funciones_Casino as fb
+
+capital= 1000
+respuesta_jugador=True
+pidio_mano = True
+#Preparamos la mesa y el mazo
+fb.nuevo_mazo()
+fb.preparar_mesa()
+print("¡A perder dinero con el BlackJack!\n Cuentas con un capital base de  $1000, el pago por BlackJack es de 3:2.\n")
+
+#El juego se repetirá hasta que el jugador decida salir o se quede sin capital.
+while respuesta_jugador or capital <= 0:
+    #Eleginos la apuesta del jugador con un minimo de $10 y un máximo de $500 por mano.
+    apuesta_base = int(input("¿Cuánto deseas apostar por mano? (mínimo $10, máximo $500): "))
+    while apuesta_base < 10 or apuesta_base > 500:
+        print("Apuesta inválida. Por favor, ingresa un monto entre $10 y $500.")
+        apuesta_base = int(input("¿Cuánto deseas apostar por mano? (mínimo $10, máximo $500): "))
+
+    #Repartimos manos
+    jugador, dealer = fb.repartir_manos_inicio()
+
+    #Calculamos el valor de las manos iniciales
+    # Si el jugador tiene BlackJack, gana automáticamente.
+    if fb.calcular_mano(jugador)[0] == 21:
+        print(f'cartas jugador: {jugador} \n')
+        print("¡BlackJack! Has ganado 1.5 veces tu apuesta.")
+        capital += int(apuesta_base * 1.5)  
+    else:
+        #Mostramos las cartas del dealer y del jugador, ocultando la segunda carta del dealer.
+        print(f'Cartas dealer: {dealer[0]} y ⍍ \n ')
+        print(f'Cartas jugador: {jugador[0]} y {jugador[1]} \n ¿Qué deseas hacer? ')
+        print("1. Plantarse")
+        print("2. Pedir carta")
+        print("3. Doblar")
+        if jugador[0] == jugador[1]:
+            print("4. Dividir")
+
+        #Pedimos la acción del jugador.
+        respuesta= int(input())
+        while respuesta not in [1, 2, 3, 4] or (respuesta == 4 and jugador[0] != jugador[1]):
+            print("Opción inválida. Por favor, elige una opción válida.")
+            respuesta= int(input())
+
+        #Procesamos la acción del jugador.
+        if respuesta == 1:
+            fb.plantarse(jugador,dealer,apuesta_base,capital)
+            respuesta_jugador = input("¿Deseas jugar otra mano? (s/n): ").lower() == 's'
+        elif respuesta == 2:
+            print("Has decidido pedir una carta.")
+            while pidio_mano and fb.calcular_mano(jugador)[0] <= 21:
+                jugador = fb.dar_carta_jugador(jugador)
+                if fb.calcular_mano(jugador)[0] > 21:
+                    capital -= apuesta_base
+                    print(f'Cartas jugador: {jugador} \n ¡Has perdido! Tu capital actual es: ${capital}')
+                    respuesta_jugador = input("¿Deseas jugar otra mano? (s/n): ").lower() == 's'
+                    break
+                else:
+                    print(f'Cartas jugador: {jugador} \n ¿Deseas pedir otra carta? (s/n): ')
+                    pidio_mano = input().lower() == 's'
+            if not pidio_mano:
+                fb.plantarse(jugador,dealer,apuesta_base,capital)
+                respuesta_jugador = input("¿Deseas jugar otra mano? (s/n): ").lower() == 's'
+        
+
+            
---
+++ Funciones_Casino.py
@@ -0,0 +1,90 @@
+import random
+import time
+
+#Preparamos un mazo de 6 barajas (312 cartas)
+tipos_cartas = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+mazo = tipos_cartas * 24
+
+valores_baraja = {
+    '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 
+    'J': 10, 'Q': 10, 'K': 10, 'A': 11
+}
+
+#Funcion para barajear las cartas
+def preparar_mesa():
+    tiempo_actual = time.time()
+    random.seed(tiempo_actual) 
+    random.shuffle(mazo)
+
+#Funcion para crear un nuevo mazo de cartas (6 barajas) y barajearlo.
+def nuevo_mazo():
+    global mazo
+    mazo = tipos_cartas * 24
+    preparar_mesa()
+    return mazo
+
+#Funcion para obtener una carta del mazo.
+#   de tener un numero menor o igual a 70, vuelve a hacer un nuevo mazo y barajearlo.
+def obtener_carta():
+    if len(mazo) <= 70:
+        nuevo_mazo()
+        preparar_mesa()
+        return mazo.pop()
+    return mazo.pop() 
+
+#Funcion para repartir manos
+def repartir_manos_inicio():
+    mano_jugador = [obtener_carta(), obtener_carta()]
+    mano_dealer = [obtener_carta(), obtener_carta()]
+    return mano_jugador, mano_dealer
+
+#Funcion para dar carta adicional a jugador
+def dar_carta_jugador(mano_jugador):
+    mano_jugador.append(obtener_carta())
+    return mano_jugador
+
+#Funcion para dar carta adicional a dealer
+def dar_carta_dealer(mano_dealer):
+    mano_dealer.append(obtener_carta())
+    return mano_dealer
+
+#calculamos el valor de la mano.
+def calcular_mano(mano):
+    total = 0
+    ases = 0
+    for carta in mano:
+        valor = valores_baraja[carta]
+        total += valor
+        if carta == 'A': ases += 1
+    while total > 21 and ases > 0:
+        total -= 10
+        ases -= 1   
+    return total, (ases > 0)
+
+#Evaluamos las cartas del dealer 
+def evaluacion_mano_dealer(mano_dealer):
+    while True:
+        valor, suave = calcular_mano(mano_dealer)
+        if valor < 17 or (valor == 17 and suave):
+            mano_dealer.append(obtener_carta())
+        else:
+            break
+    return mano_dealer
+
+#Acciones jugador
+def plantarse(jugador,dealer,apuesta_base,capital):
+    print("Has decidido plantarte.")
+    print('La mano del dealer es: ')
+    print(evaluacion_mano_dealer(dealer))
+    if calcular_mano(dealer)[0] > 21 or calcular_mano(jugador)[0] > calcular_mano(dealer)[0]:
+        print("¡Has ganado!")
+        capital += apuesta_base
+        print(f"Tu capital actual es: ${capital}")
+    elif calcular_mano(jugador)[0] < calcular_mano(dealer)[0]:
+        print("Has perdido.")
+        capital -= apuesta_base
+        print(f"Tu capital actual es: ${capital}")
+    else:
+        print("Empate.")
+        print(f"Tu capital actual es: ${capital}")
+    return capital
